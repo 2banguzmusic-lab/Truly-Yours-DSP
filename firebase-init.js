ab76fb981ed5b335f650137a37f8016a95407183
2banguzmusic-lab <2banguzmusic@gmail.com>
2025-12-31 15:14:39 -0500

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,14 +1,16 @@
-// --- Firebase Initialization (Truly Yours DSP v2) ---
+// firebase-init.js
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-storage.js";
 
 export const firebaseConfig = {
   apiKey: "AIzaSyCyt3DsYE6fE4ZARXoE2uP5ddWg2OvwLxs",
   authDomain: "truly-yours-dsp-v2.firebaseapp.com",
   projectId: "truly-yours-dsp-v2",
-  storageBucket: "truly-yours-dsp-v2.appspot.com", 
+  storageBucket: "truly-yours-dsp-v2.appspot.com",
   messagingSenderId: "26187926000",
   appId: "1:26187926000:web:7aa5ede1559bf2fec3f535"
 };
 
 export const app = initializeApp(firebaseConfig);
-
+export const storage = getStorage(app);
